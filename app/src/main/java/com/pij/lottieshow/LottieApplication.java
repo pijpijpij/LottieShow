24e14b22ed0730ce229fedf7f3c7517a9e9aa1e1
pijpijpij <atlassian@champault.co.uk>
2017-04-20 10:28:39 +0100

    Feat: added Fabric (only local build can be deployed to Beta).

--- app/src/main/java/com/pij/lottieshow/LottieApplication.java
+++ app/src/main/java/com/pij/lottieshow/LottieApplication.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.support.v4.app.Fragment;
 
+import com.crashlytics.android.Crashlytics;
 import com.squareup.leakcanary.LeakCanary;
 
 import javax.inject.Inject;
@@ -11,6 +12,7 @@ import javax.inject.Inject;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasDispatchingActivityInjector;
 import dagger.android.support.HasDispatchingSupportFragmentInjector;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * <p>Created on 08/04/2017.</p>
@@ -35,6 +37,13 @@ public class LottieApplication extends Application
             return;
         }
         LeakCanary.install(this);
+
+        if (BuildConfig.USE_FABRIC) {
+            Fabric.with(this, new Crashlytics());
+            System.out.println("Fabric enabled.");
+        } else {
+            System.out.println("Fabric disable.");
+        }
 
         DaggerLottieApplicationComponent.builder().application(this).build().inject(this);
     }
